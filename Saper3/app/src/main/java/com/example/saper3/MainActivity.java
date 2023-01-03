15fdef6e235a8a67cb419343a149e630bfd5cbb3
Maria Malasiewicz <malasie@users.noreply.github.com>
2023-01-03 21:12:55 +0100

    update

--- Saper3/app/src/main/java/com/example/saper3/MainActivity.java
+++ Saper3/app/src/main/java/com/example/saper3/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     int wiel= 6;
     Random liczba= new Random();
     int miny= liczba.nextInt(3)+5;  //liczba min
+    int l_flag=0;
 
     ArrayList<Integer> X= new ArrayList<>();        //listy współrzędnych min
     ArrayList<Integer> Y= new ArrayList<>();
@@ -71,63 +72,18 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-    void Otoczenie( int w, int k){
-        int m = 0;
-        for (int a = 0; a < X.size(); a++) {              //sprawdzamy ile min jest w okolicy przycisku
-            int x = X.get(a);
-            int y = Y.get(a);
-            if (x == w - 1 & y == k - 1) {
-                m++;
-            }
-            if (x == w - 1 & y == k) {
-                m++;
-            }
-            if (x == w - 1 & y == k + 1) {
-                m++;
-            }
-            if (x == w & y == k - 1) {
-                m++;
-            }
-            if (x == w & y == k + 1) {
-                m++;
-            }
-            if (x == w + 1 & y == k - 1) {
-                m++;
-            }
-            if (x == w + 1 & y == k) {
-                m++;
-            }
-            if (x == w + 1 & y == k + 1) {
-                m++;
-            }
-        }
-        // dodaj tekst "m"
-    }
-
 
     boolean flaga = false;
 
     ArrayList<Integer> FlagiX= new ArrayList<Integer>();
     ArrayList<Integer> FlagiY= new ArrayList<Integer>();
 
-    void Postaw(int w, int k) {
-        for (int b = 0; b < FlagiX.size(); b++){
-            if (FlagiX.get(b) == w && FlagiY.get(b)== k ){
-                FlagiX.remove(b);
-                FlagiY.remove(b);
-            }
-            else {
-                FlagiX.add(w);
-                FlagiY.add(k);
-            }
-        }
-    }
 
 
     boolean znalezione() {
         int l = 0;
         for (int b = 0; b < FlagiX.size(); b++){
-            for (int a = 0; a < X.size(); a++){
+            for (int a = 0; a < miny; a++){
                 if (FlagiX.get(b) == X.get(a) && FlagiY.get(b) == Y.get(a) ){
                     l++;
                 }
@@ -167,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
-                if (l == TextIds.length) {                              //i liczba min jest równa liczbie odblokowanych przycisków
+                if (l == TextIds.length - miny) {                              //i liczba min jest równa liczbie odblokowanych przycisków
                     return true;                                    // zwraca true (wygrana)
                 }
                 else return false;
@@ -180,10 +136,23 @@ public class MainActivity extends AppCompatActivity {
 
     void Polecenie(int w, int k){
         if (flaga == true){
-            Postaw(w, k);
-            // obrazek flagi
             ImageView flaga = findViewById(Wspolrzedne(w,k,"iv"));
-            flaga.setVisibility(View.VISIBLE);
+            if (flaga.getVisibility()==View.VISIBLE) {
+                flaga.setVisibility(View.INVISIBLE);
+                for(int i=0; i<l_flag; i++){
+                    if(FlagiX.get(i)==w && FlagiY.get(i)==k){
+                        FlagiX.remove(i);
+                        FlagiY.remove(i);
+                    }
+                }
+                l_flag-=1;
+            }
+            else {
+                flaga.setVisibility(View.VISIBLE);
+                l_flag+=1;
+                FlagiX.add(w);
+                FlagiY.add(k);
+            }
         }
         else{
             if (sprawdz(w, k) == true){
@@ -300,6 +269,8 @@ public class MainActivity extends AppCompatActivity {
     }
     public void Klikniecie(View view){
         int id=view.getId();
+        Log.i("liczba flag", Integer.toString(FlagiX.size())+" liczba min "+ Integer.toString(miny));
+
         if(id == R.id.button){
             flaga= ! flaga;
             CheckBox checkBox = findViewById(R.id.checkBox);
@@ -326,6 +297,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void NewGame(View view){
+        l_flag=0;
+        int [] ImageViewIds = new int [] {R.id.iv0_0, R.id.iv0_1,R.id.iv0_2,R.id.iv0_3,R.id.iv0_4,R.id.iv0_5,
+                R.id.iv1_0,R.id.iv1_1,R.id.iv1_2,R.id.iv1_3,R.id.iv1_4,R.id.iv1_5,
+                R.id.iv2_0, R.id.iv2_1,R.id.iv2_2,R.id.iv2_3,R.id.iv2_4,R.id.iv2_5,
+                R.id.iv3_0,R.id.iv3_1,R.id.iv3_2,R.id.iv3_3,R.id.iv3_4,R.id.iv3_5,
+                R.id.iv4_0, R.id.iv4_1,R.id.iv4_2,R.id.iv4_3,R.id.iv4_4,R.id.iv4_5,
+                R.id.iv5_0,R.id.iv5_1,R.id.iv5_2,R.id.iv5_3,R.id.iv5_4,R.id.iv5_5};
         int [] TextIds = new int [] {R.id.t0_0, R.id.t0_1,R.id.t0_2,R.id.t0_3,R.id.t0_4,R.id.t0_5,
                 R.id.t1_0,R.id.t1_1,R.id.t1_2,R.id.t1_3,R.id.t1_4,R.id.t1_5,
                 R.id.t2_0, R.id.t2_1,R.id.t2_2,R.id.t2_3,R.id.t2_4,R.id.t2_5,
@@ -335,6 +313,8 @@ public class MainActivity extends AppCompatActivity {
         for ( int i=0; i< TextIds.length; i++){
             TextView tekst=(TextView)findViewById(TextIds[i]);
             tekst.setVisibility(View.INVISIBLE);
+            ImageView image=(ImageView)findViewById(ImageViewIds[i]);
+            image.setVisibility(View.INVISIBLE);
         }
         Miny();
         ImageView Boom = findViewById(R.id.gif);
@@ -342,6 +322,8 @@ public class MainActivity extends AppCompatActivity {
         Button newGame = findViewById(R.id.newGame);
         newGame.setVisibility(View.INVISIBLE);
         mediaPlayer.start();
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setChecked(false);
 
     }
 }
